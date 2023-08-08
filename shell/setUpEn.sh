94043a049a4c3cf50156529e862b3aa27731f163
Holly Giles <harg2@mti-ai-srv-03.jcbc.private.cam.ac.uk>
2023-08-08 16:51:07 +0100

    add script for setting up conda environment

---
+++ shell/setUpEn.sh
@@ -0,0 +1,6 @@
+conda create -n mageck-vispr python=3.7
+conda activate mageck-vispr
+conda install -c conda-forge mamba
+conda install -c bioconda -c conda-forge mageck
+mamba install -c bioconda -c conda-forge mageck-vispr
+
